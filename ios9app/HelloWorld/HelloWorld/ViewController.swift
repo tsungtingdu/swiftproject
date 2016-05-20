56e2e587a12bf4cacfe6c709481c382287391ddf
Tsung-Ting Du <tsungtingdu@gmail.com>
2016-05-20 21:58:12 +0800

    first commit

---
+++ ios9app/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Canopus on 2016/5/20.
+//  Copyright © 2016年 Canopus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

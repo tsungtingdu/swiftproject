46cebe94a6edf4d9ec075993be12f3d32fc77c76
Tsung-Ting Du <tsungtingdu@gmail.com>
2016-05-23 13:02:34 +0800

    delete BullsAndCows and BullsAndCowsProject

--- ios9app/HelloWorld.playground/Contents.swift
+++ ios9app/HelloWorld.playground/Contents.swift
@@ -3,3 +3,24 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var intSet:Set=[1,2]
+intSet.insert(10)
+intSet.insert(9)
+intSet.insert(8)
+intSet.insert(7)
+intSet.insert(8)
+
+
+var str2 = "Let me know!!"
+
+
+for i in 0...5{
+
+    print("\(i)")
+}
+
+var sum = 0
+for integer in 1...10 where integer % 2 == 0 {
+    sum=sum + integer
+}
